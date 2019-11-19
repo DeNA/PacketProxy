ad9a89f49f3652250dc04530b8929b8322f724da
funa-tk <1781263+funa-tk@users.noreply.github.com>
2019-11-19 19:15:07 +0900

    [WIP] update

--- src/main/java/core/packetproxy/http2/frames/FrameBase.java
+++ src/main/java/core/packetproxy/http2/frames/FrameBase.java
@@ -1,10 +1,41 @@
 package packetproxy.http2.frames;
 
+import java.io.ByteArrayInputStream;
+
+import org.apache.commons.codec.binary.Hex;
+
 public class FrameBase {
-    private byte length[] = new byte[3];
-    private static int type = 0x04;
-    private byte flags = 0;
-    private boolean r = false;
-    private byte streamId[] = new byte[4];
-    private byte framePayload[];
+    private int length;
+    private int type;
+    private int flags;
+    private int streamId;
+    private byte[] payload;
+    
+    public static void main(String[] args) throws Exception {
+    	byte[] frame = Hex.decodeHex("00001B010500000001828487418798E79A82AE43D37A8825B650C3ABB6D2E053032A2F2A".toCharArray());
+    	FrameBase fb = new FrameBase(frame);
+    	System.out.println(fb.toString());
+    }
+    
+    public FrameBase(byte[] data) throws Exception {
+    	ByteArrayInputStream bais = new ByteArrayInputStream(data);
+    	byte[] buffer = new byte[1024];
+
+    	bais.read(buffer, 0, 3);
+    	length = (int)(buffer[0] << 16 | buffer[1] << 8 | buffer[2]);
+    	bais.read(buffer, 0, 1);
+    	type = (int)buffer[0];
+    	bais.read(buffer, 0, 1);
+    	flags = (int)buffer[0];
+    	bais.read(buffer, 0, 4);
+    	streamId = (int)((buffer[0] & 0x7f) << 24 | buffer[1] << 16 | buffer[2] << 8 | buffer[3]);
+    	payload = new byte[length];
+    	bais.read(payload);
+    }
+    
+    @Override
+    public String toString() {
+    	return String.format("length=%d, type=%d, flags=%d, streamId=%d, data=%s", length, type, flags, streamId, new String(Hex.encodeHex(payload)));
+    }
+    
 }
