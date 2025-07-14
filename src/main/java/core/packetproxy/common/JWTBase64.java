24195b64d0a021859337189c43078528ba8cabe7
roaris <one0803kyopro@gmail.com>
2025-07-14 16:38:41 +0900

    VulCheck Helperで生成されるalg=noneのJWTの末尾に.が無いのを修正

--- src/main/java/core/packetproxy/common/JWTBase64.java
+++ src/main/java/core/packetproxy/common/JWTBase64.java
@@ -37,9 +37,8 @@ public class JWTBase64 extends JWT {
 		sb.append(createPayload(this.payload));
 		String header_payload = sb.toString();
 		String signature = createSignature(header_payload);
+		sb.append(".");
 		if (!signature.isEmpty()) {
-
-			sb.append(".");
 			sb.append(signature);
 		}
 		return sb.toString();
