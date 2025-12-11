3a333126c0ab528fc7c2b7e664b794e82a25e53c
ryosuke.a.tanaka <ryosuke.a.tanaka@dena.com>
2025-12-11 16:46:57 +0900

    add: mode別動的補完機能

---
+++ src/main/kotlin/core/packetproxy/gulp/DynamicCompleter.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2025 DeNA Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package packetproxy.gulp
+
+import org.jline.reader.Candidate
+import org.jline.reader.Completer
+import org.jline.reader.LineReader
+import org.jline.reader.ParsedLine
+import packetproxy.cli.CLIModeHandler
+
+/** 動的にモードに応じて補完を変更するCompleter */
+class DynamicCompleter(private var handler: CLIModeHandler) : Completer {
+  fun updateHandler(newHandler: CLIModeHandler) {
+    handler = newHandler
+  }
+
+  override fun complete(reader: LineReader, line: ParsedLine, candidates: MutableList<Candidate>) {
+    handler.completer.complete(reader, line, candidates)
+  }
+}
