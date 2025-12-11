66d11c05658af9ea13e50d999a61f195dfab6668
ryosuke.a.tanaka <ryosuke.a.tanaka@dena.com>
2025-12-11 12:39:54 +0900

    update: convert java to kotlin

--- src/main/kotlin/core/packetproxy/ProxyFactory.kt
+++ src/main/kotlin/core/packetproxy/ProxyFactory.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 DeNA Co., Ltd.
+ * Copyright 2025 DeNA Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,68 +13,47 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package packetproxy;
+package packetproxy
 
-import static packetproxy.util.Logging.log;
+import java.net.ServerSocket
+import packetproxy.common.I18nString
+import packetproxy.model.ListenPort
+import packetproxy.util.Logging
 
-import java.net.ServerSocket;
-import packetproxy.common.I18nString;
-import packetproxy.model.ListenPort;
+object ProxyFactory {
+  @JvmStatic
+  @Throws(Exception::class)
+  fun create(listenInfo: ListenPort): Proxy {
+    val type = listenInfo.getType()
+    val port = listenInfo.getPort()
 
-public class ProxyFactory {
+    Logging.log("type is $type")
+    Logging.log(I18nString.get("Start listening port %d.", port))
 
-	public static Proxy create(ListenPort listen_info) throws Exception {
-		Proxy proxy = null;
-		if (listen_info.getType() == ListenPort.TYPE.HTTP_PROXY) {
+    return when (type) {
+      ListenPort.TYPE.UDP_FORWARDER -> ProxyUDPForward(listenInfo)
+      ListenPort.TYPE.QUIC_FORWARDER -> ProxyQuicForward(listenInfo)
+      ListenPort.TYPE.QUIC_TRANSPARENT_PROXY -> ProxyQuicTransparent(listenInfo)
 
-			ServerSocket listen_socket = new ServerSocket(listen_info.getPort());
-			proxy = new ProxyHttp(listen_socket, listen_info);
+      else -> {
+        val listenSocket = ServerSocket(port)
 
-		} else if (listen_info.getType() == ListenPort.TYPE.SSL_FORWARDER) {
+        when (type) {
+          ListenPort.TYPE.HTTP_PROXY -> ProxyHttp(listenSocket, listenInfo)
+          ListenPort.TYPE.SSL_FORWARDER -> ProxySSLForward(listenSocket, listenInfo)
+          ListenPort.TYPE.HTTP_TRANSPARENT_PROXY -> ProxyHttpTransparent(listenSocket, listenInfo)
+          ListenPort.TYPE.SSL_TRANSPARENT_PROXY -> ProxySSLTransparent(listenSocket, listenInfo)
 
-			log("type is SSL_FORWARDER");
-			ServerSocket listen_socket = new ServerSocket(listen_info.getPort());
-			proxy = new ProxySSLForward(listen_socket, listen_info);
+          else -> {
+            listenSocket.setReuseAddress(true)
 
-		} else if (listen_info.getType() == ListenPort.TYPE.HTTP_TRANSPARENT_PROXY) {
-
-			log("type is HTTP_TRANSPARENT_PROXY");
-			ServerSocket listen_socket = new ServerSocket(listen_info.getPort());
-			proxy = new ProxyHttpTransparent(listen_socket, listen_info);
-
-		} else if (listen_info.getType() == ListenPort.TYPE.SSL_TRANSPARENT_PROXY) {
-
-			log("type is SSL_TRANSPARENT_PROXY");
-			ServerSocket listen_socket = new ServerSocket(listen_info.getPort());
-			proxy = new ProxySSLTransparent(listen_socket, listen_info);
-
-		} else if (listen_info.getType() == ListenPort.TYPE.UDP_FORWARDER) {
-
-			proxy = new ProxyUDPForward(listen_info);
-
-		} else if (listen_info.getType() == ListenPort.TYPE.QUIC_FORWARDER) {
-
-			proxy = new ProxyQuicForward(listen_info);
-
-		} else if (listen_info.getType() == ListenPort.TYPE.QUIC_TRANSPARENT_PROXY) {
-
-			proxy = new ProxyQuicTransparent(listen_info);
-
-		} else if (listen_info.getType() == ListenPort.TYPE.XMPP_SSL_FORWARDER) {
-
-			log("type is XMPP_SSL_FORWARDER");
-			ServerSocket listen_socket = new ServerSocket(listen_info.getPort());
-			listen_socket.setReuseAddress(true);
-			proxy = new ProxyXmppSSLForward(listen_socket, listen_info);
-
-		} else {
-			/* FORWARDER */
-
-			ServerSocket listen_socket = new ServerSocket(listen_info.getPort());
-			listen_socket.setReuseAddress(true);
-			proxy = new ProxyForward(listen_socket, listen_info);
-		}
-		log(I18nString.get("Start listening port %d.", listen_info.getPort()));
-		return proxy;
-	}
+            when (type) {
+              ListenPort.TYPE.XMPP_SSL_FORWARDER -> ProxyXmppSSLForward(listenSocket, listenInfo)
+              else -> ProxyForward(listenSocket, listenInfo)
+            }
+          }
+        }
+      }
+    }
+  }
 }
