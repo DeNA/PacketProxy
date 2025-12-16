71b5edf316aed76242ed8a4252336d1fd97822f2
Hiroki Tokunaga <hiroki.tokunaga@dena.com>
2025-12-16 12:11:21 +0900

    WIP: migrate util nullness to JSpecify

--- src/main/java/core/packetproxy/util/Throwing.java
+++ src/main/java/core/packetproxy/util/Throwing.java
@@ -1,33 +1,37 @@
 package packetproxy.util;
 
+import java.io.UncheckedIOException;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import javax.annotation.Nonnull;
 
 public class Throwing {
 
 	private Throwing() {
 	}
 
-	@Nonnull
-	public static <T> Consumer<T> rethrow(@Nonnull final ThrowingConsumer<T> consumer) {
+	public static <T> Consumer<T> rethrow(final ThrowingConsumer<T> consumer) {
 		return consumer;
 	}
 
-	@Nonnull
-	public static <T> Predicate<T> rethrowP(@Nonnull final ThrowingPredicate<T> predicate) {
+	public static <T> Predicate<T> rethrowP(final ThrowingPredicate<T> predicate) {
 		return predicate;
 	}
 
-	@Nonnull
-	public static <T, R> Function<T, R> rethrowF(@Nonnull final ThrowingFunction<T, R> function) {
+	public static <T, R> Function<T, R> rethrowF(final ThrowingFunction<T, R> function) {
 		return function;
 	}
 
-	@SuppressWarnings("unchecked")
-	@Nonnull
-	public static <E extends Throwable> void sneakyThrow(@Nonnull Throwable ex) throws E {
-		throw (E) ex;
+	public static void sneakyThrow(Throwable ex) {
+		if (ex instanceof RuntimeException runtimeException) {
+			throw runtimeException;
+		}
+		if (ex instanceof Error error) {
+			throw error;
+		}
+		if (ex instanceof java.io.IOException ioException) {
+			throw new UncheckedIOException(ioException);
+		}
+		throw new RuntimeException(ex);
 	}
 }
